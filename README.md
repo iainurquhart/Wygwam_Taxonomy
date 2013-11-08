31f54bb93f7230132ac734f9e6568bbb69742a2b
Iain Urquhart <iain@jamdigital.co.nz>
2013-11-09 11:13:09 +1300

    First Commit, v1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# Wygwam Taxonomy Links
+
+### Overview
+
+Install this extension to have your Taxonomy Trees show up as link types in Pixel and Tonic's Wygwam Editor
+
+
+Copyright (c) 2013 Iain Urquhart
+http://iain.co.nz
---
+++ system/expressionengine/third_party/wygwam_taxonomy/ext.wygwam_taxonomy.php
@@ -0,0 +1,133 @@
+<?php if (! defined('APP_VER')) exit('No direct script access allowed');
+
+if( !function_exists('ee') )
+{
+	function ee()
+	{
+		static $EE;
+		if ( ! $EE) $EE = get_instance();
+		return $EE;
+	}
+}
+
+
+/**
+ * Wygwam Taxonomy Links
+ * 
+ * @author    Iain Urquhart <shout@iain.co.nz>
+ * @copyright Copyright (c) 2013 Iain Urquhart
+ * @license   http://creativecommons.org/licenses/by-sa/3.0/ Attribution-Share Alike 3.0 Unported
+ */
+class Wygwam_taxonomy_ext {
+
+	var $name           = 'Wygwam Taxonomy Links';
+	var $version        = '1.0';
+	var $description    = 'Adds a Taxonomy Link Type to Wygwam’s Link dialog';
+	var $settings_exist = 'y';
+	var $docs_url       = '';
+	var $settings       = array();
+
+	/**
+	 * Class Constructor
+	 */
+	function __construct($settings = '')
+    {
+        $this->settings = $settings;
+    }
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Activate Extension
+	 */
+	function activate_extension()
+	{
+		$this->settings = array(
+			'license_key'   => ''
+		);
+		// add the row to exp_extensions
+		ee()->db->insert('extensions', array(
+			'class'    => __CLASS__,
+			'method'   => 'wygwam_config',
+			'hook'     => 'wygwam_config',
+			'settings' => serialize($this->settings),
+			'priority' => 10,
+			'version'  => $this->version,
+			'enabled'  => 'y'
+		));
+	}
+
+	/**
+	 * Update Extension
+	 */
+	function update_extension($current = '')
+	{
+		// Nothing to change...
+		return FALSE;
+	}
+
+	/**
+	 * Disable Extension
+	 */
+	function disable_extension()
+	{
+		// Remove all Wygwam_template_links_ext rows from exp_extensions
+		ee()->db->where('class', __CLASS__)
+		             ->delete('extensions');
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * wygwam_config hook
+	 */
+	function wygwam_config($config, $settings)
+	{
+		// If another extension shares the same hook,
+		// we need to get the latest and greatest config
+		if (ee()->extensions->last_call !== FALSE)
+		{
+			$config = ee()->extensions->last_call;
+		}
+
+		require_once PATH_THIRD .'taxonomy/models/taxonomy_model.php';
+        ee()->wygwam_taxonomy = new Taxonomy_model;
+
+        // fetch our trees
+        $trees = ee()->db->get('taxonomy_trees');
+        $index = ee()->functions->fetch_site_index();
+
+        foreach($trees->result_array() as $tree)
+        {	
+        	// set the tree
+        	ee()->wygwam_taxonomy->set_table($tree['id']);
+
+        	// fetch our nodes
+        	$nodes = ee()->wygwam_taxonomy->get_flat_tree();
+
+        	foreach($nodes as $node)
+        	{
+        		// get the full url for the node
+        		$url = ee()->wygwam_taxonomy->build_url($node);
+
+        		// is the node an insite link
+        		if (strpos($url, $index) !== FALSE)
+        		{
+        			// wrap in {path} for portability
+        			$url = '{path='.str_replace($index, '', $url).'}';
+        		}
+
+        		$link = array(
+					'label_depth' => $node['depth']+1,
+					'label'       => $node['label'],
+					'url'         => $url
+				);
+
+				$config['link_types'][$tree['label']][] = $link;
+        	}
+
+        }
+
+		return $config;
+	}
+}
